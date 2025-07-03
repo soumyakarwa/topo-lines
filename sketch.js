befa748ec52dfcee1357d073c041e454f8aa7833
Soumya_Karwa <Soumya.Karwa@thomsonreuters.com>
2025-07-03 16:06:07 -0400

    oof

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ let contourLinesByValue = {};
 let connectedContourLines = [];
 let points = [];
 let allLines = [];
+let closedLines = [];
 let fontSize = 64;
 
 function preload() {
@@ -14,19 +15,33 @@ function preload() {
 }
 
 function setup() {
-    // frameRate(30);
     // textSize(fontSize);
+
     createCanvas(windowWidth, windowHeight);
 
     pixelsInWidth = width / res;
     pixelsInHeight = height / res;
 
+    let noiseScale = 40;
+    let centerX = (pixelsInWidth + 1) / 2;
+    let centerY = (pixelsInHeight + 1) / 2;
+    let flatness = 0.4; // 0 = all noise, 1 = all flat
+    let base = 10;
+
     for (let i = 0; i < 1 + pixelsInWidth; i++) {
         noiseMap[i] = [];
         for (let j = 0; j < 1 + pixelsInHeight; j++) {
+            // let distToCenter = dist(i, j, centerX, centerY);
+            // let hill = map(distToCenter, 0, Math.max(centerX, centerY), 300, 0); // 40 at center, 10 at edge
+            // noiseMap[i][j] = noise(i / noiseScale, j / noiseScale) * 80 + hill;
+            // let n = noise(i / noiseScale, j / noiseScale);
+            // noiseMap[i][j] = lerp(n * 100, base, flatness) + hill;
+
             noiseMap[i][j] = noise(i / 50, j / 50) * 100;
         }
     }
+
+    noiseMap[round(width / 4)][round(height / 2)] = 55;
 
     createContourSegments(contourSegments);
 
@@ -48,26 +63,14 @@ function setup() {
         return a.lines.length - b.lines.length;
     });
 
-    let contourValues = connectedContourLines.map((c) =>
-        Number(c.contourValue)
-    );
-    contourMinValue = min(contourValues);
-    contourMaxValue = max(contourValues);
-
     allLines = connectedContourLines.flatMap((obj) => obj.lines);
     allLines = allLines.filter((line) => line.length > 2);
     allLines.sort((a, b) => {
         return a.length - b.length;
     });
 
-    points = font.textToPoints(
-        "THE EROSION",
-        width / 2 - textWidth("THE EROSION") / 2,
-        height / 2 - fontSize / 2,
-        fontSize,
-        {
-            sampleFactor: 0.9,
-        }
+    closedLines = allLines.filter(
+        (line) => line.length > 1 && pointsEqual(line[0], line[line.length - 1])
     );
 }
 
@@ -76,30 +79,62 @@ function draw() {
 
     for (let i = 0; i < allLines.length; i++) {
         // let contour = connectedContourLines[i];
+        let currentLine = allLines[i];
 
         // strokeWeight(
         //     map(contour.contourValue, contourMaxValue, contourMinValue, 0.1, 1)
         // );
-        let strokeOpacity =
-            i > frameCount ? 1 : map(frameCount, i, i * 1.5, 1, 0);
+        // let strokeOpacity =
+        //     i > frameCount ? 1 : map(frameCount, i, i * 1.25, 1, 0.5);
 
-        stroke(100, 100, 100, strokeOpacity * 255);
+        // i != round(allLines.length * 0.99)
+        //     ? stroke(100, 100, 100, 1 * 255)
+        //     : stroke(255, 0, 0, 255);
+
         noFill();
-
-        for (let j = 0; j < allLines[i].length - 1; j++) {
+        stroke(100, 100, 100, 255);
+        for (let j = 0; j < currentLine.length - 1; j++) {
+            // for ulta etch a sketch effect
             // if (j < frameCount) {
             //     stroke("#f0efdf");
             // } else {
             //     stroke(100, 100, 100);
             // }
             line(
-                allLines[i][j][0],
-                allLines[i][j][1],
-                allLines[i][j + 1][0],
-                allLines[i][j + 1][1]
+                currentLine[j][0],
+                currentLine[j][1],
+                currentLine[j + 1][0],
+                currentLine[j + 1][1]
             );
         }
     }
+
+    for (
+        let i = round(closedLines.length * 0.95);
+        i < closedLines.length;
+        i++
+    ) {
+        let currentLine = closedLines[i];
+        fill("#f0efdf");
+        noStroke();
+        beginShape();
+        for (point of currentLine) {
+            vertex(point[0], point[1]);
+        }
+        endShape(CLOSE);
+    }
+
+    // for (let i = 0; i < 1 + pixelsInWidth; i += 3) {
+    //     for (let j = 0; j < 1 + pixelsInHeight; j += 3) {
+    //         // console.log(i, j);
+    //         textSize(5);
+    //         let c = map(round(noiseMap[i][j]), 0, 100, 0, 255);
+    //         noStroke();
+    //         fill(c, 0, 0);
+    //         text(round(noiseMap[i][j]), i * res, j * res);
+    //     }
+    // }
+    // text(noiseMap[round(width / 4)][round(height / 2)], width / 4, height / 2);
 
     // if (frameCount > 60) {
     // console.log("Drawing points");
@@ -127,4 +162,14 @@ function draw() {
     //     // }
     // }
     // }
+
+    // points = font.textToPoints(
+    //     "THE EROSION",
+    //     width / 2 - textWidth("THE EROSION") / 2,
+    //     height / 2 - fontSize / 2,
+    //     fontSize,
+    //     {
+    //         sampleFactor: 0.9,
+    //     }
+    // );
 }
